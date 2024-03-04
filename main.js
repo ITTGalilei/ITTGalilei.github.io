06c9c8b9b29c8ca9e8e7cb023269ffcff07d339f
AleStoCactus <alessandropala06@gmail.com>
2024-03-04 01:04:58 +0100

    Hotfix 0.2.1 - Menu bottom
    Fixed the menu bottom, now it's a little bit more responsive.

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 function home() {
 	window.location.assign("index.html");
 }
+
+function chisiamo() {
+	window.location.assign("chisiamo.html");
+}
+function cosarealiziamo() {
+	window.location.assign("cosarealiziamo.html");
+}
+function collaboratori() {
+	window.location.assign("collaboratori.html");
+}
